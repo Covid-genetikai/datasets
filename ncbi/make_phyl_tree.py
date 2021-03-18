d601e1fffe3190154ddc39614fdf233405b5685c
Mantas Stankevicius <mantas.stankevicius@cern.ch>
2021-03-18 21:43:22 +0100

    make phylogenetic tree

---
+++ ncbi/make_phyl_tree.py
@@ -0,0 +1,38 @@
+from Bio import Phylo
+from Bio.Phylo.TreeConstruction import DistanceCalculator
+from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
+from Bio import AlignIO
+
+import time
+from pathlib import Path
+
+def main():
+
+    path = Path("good_unique2")
+    # path = Path("good_unique_small")
+
+    # Read aligned sequences
+    aln = AlignIO.read(path.with_suffix(".aln"), 'fasta')
+
+    # Tree making magic
+    calculator = DistanceCalculator('identity')
+    dm = calculator.get_distance(aln)
+    constructor = DistanceTreeConstructor()
+    tree = constructor.upgma(dm)
+
+    # Save tree in ascii
+    Phylo.draw_ascii(tree, open(path.with_suffix(".ascii"), "w"))
+
+    # Save tree in dnd
+    Phylo.write(tree, path.with_suffix(".dnd"), format='newick')
+
+    # Show tree image
+    Phylo.draw(tree)
+
+
+if __name__ == "__main__":
+    t0 = time.process_time()
+
+    main()
+
+    print(f"Time taken {int(time.process_time() - t0)} sec")
